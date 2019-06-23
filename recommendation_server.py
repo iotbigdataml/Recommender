57078823985caea5b294f63a5ecf74f0c9f1a114
vishwas <vishwass@andrew.cmu.edu>
2019-06-23 03:06:19 -0400

    Clean up

--- recommendation_server.py
+++ recommendation_server.py
@@ -2,42 +2,30 @@ from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from flask_cors import CORS, cross_origin
 from json import dumps, loads, load
-import http.client, urllib, urllib.request, urllib.parse, urllib.error, base64, re, json, pymysql
-import requests
+import http.client, urllib, urllib.request, urllib.parse, urllib.error, base64, re, json, pymysql, urllib2,requests
 
 app = Flask(__name__)
 api = Api(app)
 cors = CORS(app)
 app.debug = True
 
-db = pymysql.connect(host="ediss.cj2pus6demaz.us-east-1.rds.amazonaws.com", user="root", passwd="admin123", db="login")
-
+fulfillmentServiceurl = ""
 class Notify(Resource):
     def get(self):
+        json.load(urllib2.urlopen(fulfillmentServiceurl))
         self.createPallets(self.readDB())
-        #strjson = '[{\"orderid\":1,\"item\":\"Blue\",\"inventory\":9},{\"orderid\":2,\"item\":\"Red\",\"inventory\":3},{\"orderid\":3,\"item\":\"Yellow\",\"inventory\":4}]'
-        #return strjson
         return "noted"
     
     def post(self):
         return "Processed"
 
     def readDB(self):
-        # url = "http://maps.googleapis.com/maps/api/geocode/json?address=googleplex&sensor=false"
-        # r = requests.get(url)
-        cursor = db.cursor()
-        sql = "select * from account"
-        print(cursor.execute(sql))
-        rows = cursor.fetchall()
+        
         return rows #modify this
     
     def createPallets(self,orders):
         #intelligent logic goes here
-        #for(order:orders)
-            #insertQuery = ""
-            #cursor = db.cursor()
-            #result  = cursor.execute(insertQuery)
-            #db.commit()
+        # call insert API from Orer fulfillment
         return "pallets"
 
 class GetPallet(Resource):
@@ -48,19 +36,8 @@ class GetPallet(Resource):
         return "Processed"
 
 def getPallet():
-        print(db)
-        cursor = db.cursor()
-        sql = "select * from account"
-        print(cursor.execute(sql))
-        rows = cursor.fetchall()
-        print(type(rows))
-        # Convert query to row arrays
-        rowarray_list = []
-        # for row in rows:
-        #     rowarray_list.append(row)
-        orderid = 123 #get from rows
-        markread(orderid)
-        return json.dumps(rows)
+        #implement if needed
+        return "NOT IMPLEMENTED"
 
 def markread(orderid):
         print(orderid)
